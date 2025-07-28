807dc7809170198f68cfe1be00e74b828058c448
HEMANTHREDDYALLA <hemanthreddy7050@gmail.com>
2025-07-28 10:43:54 +0530

    Deploy React app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // Replace "reactcicd" with your actual repo name!
 export default defineConfig({
-  base: '/https://github.com/HEMANTHREDDYALLA/reactcicd.git/',
+  base: '/reactcicd/',
   plugins: [react()],
 })
